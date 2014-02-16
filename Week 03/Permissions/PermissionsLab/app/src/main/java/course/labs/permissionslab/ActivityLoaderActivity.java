9991cc5291efec2fc641ab806c5ec0d915094f5f
XenoPhex <AnandGaitonde@Gmail.com>
2014-02-16 13:48:44 -0500

    Changes for getting TestBookmarks functional.

--- Week 03/Permissions/PermissionsLab/app/src/main/java/course/labs/permissionslab/ActivityLoaderActivity.java
+++ Week 03/Permissions/PermissionsLab/app/src/main/java/course/labs/permissionslab/ActivityLoaderActivity.java
@@ -32,7 +32,6 @@ public class ActivityLoaderActivity extends Activity {
 		
 		Log.i(TAG, "Entered startBookMarksActivity()");
 	
-		// TODO - Start the BookmarksActivity
-
+		startActivity(new Intent(ActivityLoaderActivity.this, BookmarksActivity.class));
 	}
 }
